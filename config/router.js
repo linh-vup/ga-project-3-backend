fffb5d4cdfffad2cd08c011992036be224391a54
Joel <joelsahiti@yahoo.co.uk>
2023-01-03 16:50:19 +0000

    secureRout Added

--- config/router.js
+++ config/router.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import brandController from '../controllers/brandController';
+import secureRoute from '../middleware/secureRoute';
 
 const Router = express.Router();
 
 Router.route('/products');
 
-Router.route('/brands');
+Router.route('/brands')
+  .get(brandController.getAllBrands)
+  .post(secureRoute, brandController.createNewBrand);
 
 Router.route('/login');
 
---
+++ middleware/secureRoute.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+
+const secureRoute = async (req, res, next) => {
+  try {
+    const authToken = req.headers.authorization;
+
+    if (!authToken || !authToken.startsWith('Bearer')) {
+      return res.status(301).json({ message: 'Unauthorized' });
+    }
+
+    const token = authToken.substring(7);
+
+    jwt.verify(token, async (err, data) => {
+      if (err) {
+        return res.status(301).json({ message: 'Unauthorized' });
+      }
+
+      const user = await User.findById(data.userId);
+
+      if (!user) {
+        return res.status(301).json({ message: 'Unauthorized' });
+      }
+
+      req.currentUser = user;
+
+      next();
+    });
+  } catch (e) {
+    return res.status(301).json({ message: 'Unauthorized' });
+  }
+};
+
+export default secureRoute;
