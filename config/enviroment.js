57e583c0706a53f0ed7ff68a6009c95dc3e2100b
ermaitland <maitland.eleanor@gmail.com>
2023-01-03 12:11:07 +0000

    basic router done

---
+++ config/enviroment.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+
+export const DB_URI =
+  process.env.DB_URI || 'mongodb://127.0.0.1:27017/vegan-db';
+
+export const SECRET = process.env.SECRET || 'flowers';
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('project-3');
+import express from 'express';
+import Router from './config/router.js';
+import { PORT } from './config/enviroment.js';
+import { connectDb } from './db/helpers.js';
+
+const app = express();
+app.use(express.json());
+app.use('/api', Router);
+
+async function startServer() {
+  try {
+    await connectDb();
+    console.log('🧿 Connected to mongoDb');
+    app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
+  } catch (err) {
+    console.log('Error', err);
+  }
+}
+
+startServer();
