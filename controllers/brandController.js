65385fa6cdae2f81c6f1fbf998e75957df41c29b
Joel <joelsahiti@yahoo.co.uk>
2023-01-03 15:04:12 +0000

    Brand model test 1

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,10 +1,14 @@
-// import Brand from '../models/brand.js';
-// import products from '../models/Product.js';
+import Brand from '../models/brand.js';
+import products from '../models/Product.js';
 
-// async function createnewBrand(req, res, net) {
-//   try {
-//     const newBrand = await Brand.create(req.body);
-//   }
-// }
+async function createnewBrand(req, res, net) {
+  try {
+    const newBrand = await Brand.create(req.body);
 
-// console.log('Brand.js is here');
+    await products.updatemany({ _id: newBrand.products }, { $push: {} });
+  } catch (e) {
+    next(e);
+  }
+}
+
+console.log('Brand.js is here');
