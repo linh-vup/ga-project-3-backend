5573561d373f49572651b10def3175661252ef44
Joel <joelsahiti@yahoo.co.uk>
2023-01-03 14:46:23 +0000

    Brand model test 1

--- config/router.js
+++ config/router.js
@@ -4,4 +4,6 @@ const Router = express.Router();
 
 Router.route('/products');
 
+Router.route('/brands');
+
 export default Router;
---
+++ controllers/brandController.js
@@ -0,0 +1,10 @@
+// import Brand from '../models/brand.js';
+// import products from '../models/Product.js';
+
+// async function createnewBrand(req, res, net) {
+//   try {
+//     const newBrand = await Brand.create(req.body);
+//   }
+// }
+
+// console.log('Brand.js is here');
