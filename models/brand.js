f38c995e863c18992a569685d9c9ba094f7207f9
Joel <joelsahiti@yahoo.co.uk>
2023-01-04 15:36:17 +0000

    seed made woeking

--- models/brand.js
+++ models/brand.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
-import mongooseUniqueValidator from 'mongoose-unique-validator';
 
 const brandSchema = new mongoose.Schema({
   name: { type: String, required: true },
   products: [{ type: mongoose.Types.ObjectId, ref: 'Product' }]
 });
-
-brandSchema.plugin(mongooseUniqueValidator);
 
 export default mongoose.model('Brand', brandSchema);
