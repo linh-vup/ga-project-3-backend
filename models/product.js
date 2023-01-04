e70bf0e3bc694ff39ac2a13da048048d7113a0d0
linhvu <linh.vup@gmail.com>
2023-01-04 13:41:42 +0000

    add categorSchema and categoriesControllery

--- models/product.js
+++ models/product.js
@@ -10,7 +10,8 @@ const reviewSchema = new mongoose.Schema({
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true, min: 1, max: 300 },
-  category: { type: String, required: true },
+  // category: { type: mongoose.Schema.ObjectId, ref: 'Category', required: true },
+  category: { type: mongoose.Schema.ObjectId, ref: 'Category' },
   image: { type: String, required: true },
   rating: { type: Number, min: 1, max: 5 },
   addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
