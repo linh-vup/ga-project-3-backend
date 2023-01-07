c1ca9575412a326eea220f86e3ad1d899ca21593
linhvu <linh.vup@gmail.com>
2023-01-07 22:26:29 +0000

    add workaround to brand and category search

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -36,7 +36,26 @@ async function searchProducts(req, res, next) {
         ]
       });
 
-    return res.status(200).json(products);
+    const categories = await Category.find()
+      // .populate('category')
+      .find({
+        $or: [
+          { name: { $regex: search, $options: 'i' } }
+          // { category: new RegExp(search, 'i') }
+          // { category: { $regex: search, $options: 'i' } }
+          // { brand: { $regex: search, $options: 'i' } }
+        ]
+      });
+
+    return (
+      res
+        .status(200)
+        // .json(products)
+        // .json({ success: true, products: products, categories: categories })
+        .json({ products, categories })
+    );
+
+    // returns two objects with array of results inside
   } catch (error) {
     next(error);
   }
