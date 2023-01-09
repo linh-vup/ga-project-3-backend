fc49f3b93258bb1736a7a526188932c45e08712d
ermaitland <maitland.eleanor@gmail.com>
2023-01-09 09:58:01 +0000

    backend minor changes

--- models/reviews.js
+++ models/reviews.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 export const reviewSchema = new mongoose.Schema({
   text: { type: String, required: true, max: 300 },
   reviewer: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  rating: { type: Number, min: 1, max: 5, required: true }
+  rating: { type: Number, min: 1, max: 5, required: true },
+  productName: { type: String, required: true, max: 100 }
 });
 
 export default mongoose.model('Review', reviewSchema);
